e0347e4f6f3c12ca3692c704c81934d3a850231c
Clark Gaebel <cg.wowus.cg@gmail.com>
2017-10-08 12:53:12 -0400

    fewer copies

--- 2048.c
+++ 2048.c
@@ -21,8 +21,10 @@
   for(int i = 0; i < n; ++i)
 
 #define FOR_TILES \
-  for(int i = 0; i < TILES_PER_DIM; ++i) \
-  for(int j = 0; j < TILES_PER_DIM; ++j)
+  for(i8 i = 0; i < TILES_PER_DIM; ++i) \
+  for(i8 j = 0; j < TILES_PER_DIM; ++j)
+
+#define EQ(a, b) (memcmp((a), (b), sizeof(*(a))) == 0)
 
 // If a tile at (i, j) is present, tiles[i][j] contains its log_2 value.
 // If a tile at (i, j) is not present, tiles[i][j] = 0.
@@ -80,7 +82,7 @@ static void draw_tile_contents_row(const u8 row[TILES_PER_DIM], int lspace) {
   addch('\n');
 }
 
-static void print(struct game g) {
+static void print(const struct game* g) {
   int x, y;
   getmaxyx(stdscr, y, x);
   int lspace = (x - ((1 + TILE_WIDTH ) * TILES_PER_DIM + 1)) / 2;
@@ -89,50 +91,51 @@ static void print(struct game g) {
   int bot_vspace = (TILE_HEIGHT - 1) - top_vspace;
   rep('\n', tspace);
   rep(' ', x/2 - 5);
-  printw("Score: %d", g.score);
+  printw("Score: %d", g->score);
   rep('\n', 2);
   FOR(TILES_PER_DIM) {
     h_line('+', '-', lspace);
     h_lines('|', ' ', lspace, top_vspace);
-    draw_tile_contents_row(g.board.tiles[i], lspace);
+    draw_tile_contents_row(g->board.tiles[i], lspace);
     h_lines('|', ' ', lspace, bot_vspace);
   }
   h_line('+', '-', lspace);
 }
 
-static void draw(struct game g) {
+static void draw(const struct game* g) {
   move(0, 0);
   print(g);
   refresh();
 }
 
-int count_zeros(const struct board b) {
+int count_zeros(const struct board* b) {
   int zeros = 0;
-  FOR_TILES { zeros += b.tiles[i][j] == 0; }
+  FOR_TILES { zeros += b->tiles[i][j] == 0; }
   return zeros;
 }
 
-static struct board new_tile(struct board b, unsigned* seed) {
+static void new_tile(struct board* b, unsigned* seed) {
   int zeros = count_zeros(b);
   int tile = rand_r(seed) % zeros;
   u8 tile_size = 1 + (rand_r(seed) % 10 < 1);
   FOR_TILES {
-    if(b.tiles[i][j] != 0) continue;
-    if(tile-- == 0) b.tiles[i][j] = tile_size;
+    if(b->tiles[i][j] == 0 && tile-- == 0) {
+      b->tiles[i][j] = tile_size;
+      break;
+    }
   }
-  return b;
 }
 
-static struct board rotate_cw(const struct board b) {
+static void rotate_cw(struct board* b) {
   struct board r;
-  FOR_TILES { r.tiles[i][j] = b.tiles[4-j-1][i]; }
-  return r;
+  FOR_TILES { r.tiles[i][j] = b->tiles[4-j-1][i]; }
+  *b = r;
 }
 
 static int move_nonzero_first(u8 row[], int len) {
   int num_nonzero = 0;
-  FOR(len) if(row[i]) row[num_nonzero++] = row[i];
-  memset(&row[num_nonzero], 0, len - num_nonzero);
+  FOR(len) if(row[i])    row[num_nonzero++] = row[i];
+  FOR(len - num_nonzero) row[num_nonzero+i] = 0;
   return num_nonzero;
 }
 
@@ -146,26 +149,25 @@ static void merge_row_left(u8 row[TILES_PER_DIM]) {
   move_nonzero_first(row, num_nonzero);
 }
 
-static struct board merge_left(struct board b) {
-  FOR(TILES_PER_DIM) merge_row_left(b.tiles[i]);
-  return b;
+static void merge_left(struct board* b) {
+  FOR(TILES_PER_DIM)
+    merge_row_left(b->tiles[i]);
 }
 
-static bool is_victory(const struct board b) {
+static bool is_victory(const struct board* b) {
   bool r = false;
-  FOR_TILES { r |= b.tiles[i][j] >= TARGET_TILE; }
+  FOR_TILES { r |= b->tiles[i][j] >= TARGET_TILE; }
   return r;
 }
 
-static bool eq(const struct board a, const struct board b) {
-  return memcmp(&a, &b, sizeof(struct board)) == 0;
-}
-
-static bool is_loss(struct board b) {
+static bool is_loss(const struct board* b) {
   bool no_moves = true;
+  struct board rotated = *b;
   FOR(4) {
-    no_moves &= eq(b, merge_left(b));
-    b = rotate_cw(b);
+    struct board merged = rotated;
+    merge_left(&merged);
+    no_moves &= EQ(&rotated, &merged);
+    rotate_cw(&rotated);
   }
   return no_moves;
 }
@@ -180,10 +182,10 @@ static int cw_rotations_of_key(int key) {
   }
 }
 
-static int new_points(struct board b0, struct board b1) {
+static int new_points(const struct board* b0, const struct board* b1) {
   i8 dcount[TARGET_TILE+1] = {0};
-  FOR_TILES { dcount[b0.tiles[i][j]] -= 1; }
-  FOR_TILES { dcount[b1.tiles[i][j]] += 1; }
+  FOR_TILES { dcount[b0->tiles[i][j]] -= 1; }
+  FOR_TILES { dcount[b1->tiles[i][j]] += 1; }
   int score = 0;
   for(int i = TARGET_TILE; i >= 1; i--) {
     if(dcount[i] <= 0) continue;
@@ -193,23 +195,22 @@ static int new_points(struct board b0, struct board b1) {
   return score;
 }
 
-static struct game update(struct game g, int key) {
+static void update(struct game* g, int key) {
   int rotations = cw_rotations_of_key(key);
-  if(rotations < 0) return g;
+  if(rotations < 0) return;
 
-  struct board b0 = g.board;
+  struct board* board = &g->board;
+  struct board  b0    = *board;
 
   FOR(4) {
-    if(i == rotations) g.board = merge_left(g.board);
-    g.board = rotate_cw(g.board);
+    if(i == rotations) merge_left(board);
+    rotate_cw(board);
   }
 
-  g.score += new_points(b0, g.board);
+  g->score += new_points(&b0, board);
 
-  if(!eq(g.board, b0))
-    g.board = new_tile(g.board, &g.seed);
-
-  return g;
+  if(!EQ(board, &b0))
+    new_tile(board, &g->seed);
 }
 
 static void sigint(int _) {
@@ -231,14 +232,14 @@ int main(void) {
     , .score = 0
     , .seed  = time(NULL)
     };
-  FOR(INITIAL_TILES) g.board = new_tile(g.board, &g.seed);
-  while(!is_victory(g.board) && !is_loss(g.board)) {
-    draw(g);
+  FOR(INITIAL_TILES) new_tile(&g.board, &g.seed);
+  while(!is_victory(&g.board) && !is_loss(&g.board)) {
+    draw(&g);
     int key = getch();
-    g = update(g, key);
+    update(&g, key);
   }
   endwin();
   printf("You %s, with score %d!\n",
-    is_victory(g.board) ? "WIN" : "LOSE",
+    is_victory(&g.board) ? "WIN" : "LOSE",
     g.score);
 }
